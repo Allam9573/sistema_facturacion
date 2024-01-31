047fa2790718f33092c92fae9b26edf6a9721a41
Allam9573 <aallamdaniel@gmail.com>
2024-01-31 11:46:43 -0600

    first commit

---
+++ inv/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from app.models import ClaseModelo
+# Create your models here.
+
+
+class Categoria(ClaseModelo):
+    descripcion = models.CharField(
+        max_length=100, help_text='Descripcion Categoria', unique=True)
+
+    def __str__(self):
+        return f'{descripcion}'
+
+    class Meta:
+        verbose_name_plural = 'Categorias'
